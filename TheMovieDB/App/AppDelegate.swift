6458fcce14f0439b2c37ae80bdf81fc3902ca98d
lena <sue822330@gmail.com>
2021-08-28 01:57:48 +0900

    Initial Commit

---
+++ TheMovieDB/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  TheMovieDB
+//
+//  Created by Ador on 2021/08/28.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
+
---
+++ TheMovieDB/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Ador on 2021/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
